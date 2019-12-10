d7b7b82a00d0a6a591cd27711d2f76b17e89b25e
radetsky <radetsky@alkor.co.ru>
2019-12-10 08:45:13 +0400

    LCT-1664 Implemented condition to work only in config dir

--- src/main/java/org/idea/plugin/atg/action/NewAtgFileAction.java
+++ src/main/java/org/idea/plugin/atg/action/NewAtgFileAction.java
@@ -1,5 +1,6 @@
 package org.idea.plugin.atg.action;
 
+import com.intellij.facet.FacetManager;
 import com.intellij.ide.IdeView;
 import com.intellij.ide.actions.CreateFileFromTemplateAction;
 import com.intellij.ide.actions.CreateFileFromTemplateDialog;
@@ -9,10 +10,14 @@ import com.intellij.openapi.actionSystem.DataContext;
 import com.intellij.openapi.actionSystem.LangDataKeys;
 import com.intellij.openapi.actionSystem.Presentation;
 import com.intellij.openapi.fileTypes.StdFileTypes;
+import com.intellij.openapi.module.Module;
 import com.intellij.openapi.project.Project;
+import com.intellij.openapi.roots.ProjectFileIndex;
 import com.intellij.openapi.vfs.VirtualFile;
 import com.intellij.psi.PsiDirectory;
 import org.idea.plugin.atg.AtgToolkitBundle;
+import org.idea.plugin.atg.Constants;
+import org.idea.plugin.atg.module.AtgModuleFacet;
 
 import java.util.Arrays;
 
@@ -28,8 +33,8 @@ public class NewAtgFileAction extends CreateFileFromTemplateAction {
     @Override
     protected void buildDialog(Project project, PsiDirectory directory, CreateFileFromTemplateDialog.Builder builder) {
         builder.setTitle(AtgToolkitBundle.message("new.atg.file.action.dialog.title"))
-                .addKind("Repository file", StdFileTypes.XML.getIcon(), "Repository File.xml")
                 .addKind("Action file", StdFileTypes.XML.getIcon(), "Actor File.xml")
+                .addKind("Repository file", StdFileTypes.XML.getIcon(), "Repository File.xml")
                 .addKind("Pipeline file", StdFileTypes.XML.getIcon(), "Pipeline File.xml");
     }
 
@@ -42,9 +47,7 @@ public class NewAtgFileAction extends CreateFileFromTemplateAction {
     public void update(final AnActionEvent e) {
         final DataContext dataContext = e.getDataContext();
         final Presentation presentation = e.getPresentation();
-
         final boolean enabled = isAvailable(dataContext);
-
         presentation.setVisible(enabled);
         presentation.setEnabled(enabled);
     }
@@ -52,18 +55,26 @@ public class NewAtgFileAction extends CreateFileFromTemplateAction {
     @Override
     protected boolean isAvailable(final DataContext dataContext) {
         boolean available = false;
-        final Project project = CommonDataKeys.PROJECT.getData(dataContext);
+
         final IdeView view = LangDataKeys.IDE_VIEW.getData(dataContext);
+        final Project project = CommonDataKeys.PROJECT.getData(dataContext);
+        ProjectFileIndex projectFileIndex = ProjectFileIndex.getInstance(project);
+
         if (project != null && view != null && view.getDirectories().length > 0) {
-            available = Arrays.stream(view.getDirectories()).anyMatch(this::isUnderConfigDirectory);
+            available = Arrays.stream(view.getDirectories()).anyMatch(psiDirectory -> checkConfigRoot(psiDirectory.getVirtualFile(), projectFileIndex));
         }
         return available;
     }
 
-    private boolean isUnderConfigDirectory(PsiDirectory dir) {
-        VirtualFile virtualFile = dir.getVirtualFile();
-        //TODO fix ability create new ATG xml file only for config folder by info from facet perhaps
-        return virtualFile.getPath().contains("config");
-    }
+    private boolean checkConfigRoot(VirtualFile current, ProjectFileIndex projectFileIndex) {
+        Module currentModule = projectFileIndex.getModuleForFile(current);
+        VirtualFile rootDir = projectFileIndex.getSourceRootForFile(current);
 
+        AtgModuleFacet atgFacet = FacetManager.getInstance(currentModule).getFacetByType(Constants.FACET_TYPE_ID);
+
+        boolean isConfigDir = atgFacet.getConfiguration().getConfigRoots().contains(current);
+        return !isConfigDir && !current.equals(rootDir)
+                ? checkConfigRoot(current.getParent(), projectFileIndex)
+                : isConfigDir;
+    }
 }
