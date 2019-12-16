56c7e154429e58c018617f1aa062c6b965d6c5e3
Igor Pidgurskyi <Igor_Pidgurskyi@epam.com>
2019-12-16 21:44:57 +0200

    Disable "Create ATG Component on non-ATG projects"

--- src/main/java/org/idea/plugin/atg/actions/CreateATGComponentIntentionAction.java
+++ src/main/java/org/idea/plugin/atg/actions/CreateATGComponentIntentionAction.java
@@ -1,6 +1,7 @@
 package org.idea.plugin.atg.actions;
 
 import com.intellij.codeInsight.intention.PsiElementBaseIntentionAction;
+import com.intellij.facet.ProjectFacetManager;
 import com.intellij.openapi.editor.Editor;
 import com.intellij.openapi.module.Module;
 import com.intellij.openapi.module.ModuleUtilCore;
@@ -10,8 +11,8 @@ import com.intellij.psi.PsiClass;
 import com.intellij.psi.PsiElement;
 import com.intellij.psi.PsiFile;
 import com.intellij.psi.util.PsiTreeUtil;
-import com.intellij.util.IncorrectOperationException;
 import org.idea.plugin.atg.AtgToolkitBundle;
+import org.idea.plugin.atg.Constants;
 import org.idea.plugin.atg.PropertiesGenerator;
 import org.idea.plugin.atg.util.AtgComponentUtil;
 import org.jetbrains.annotations.NotNull;
@@ -32,8 +33,10 @@ public class CreateATGComponentIntentionAction extends PsiElementBaseIntentionAc
 
     @Override
     public boolean isAvailable(@NotNull Project project, Editor editor, @NotNull PsiElement element) {
+        if (!ProjectFacetManager.getInstance(project).hasFacets(Constants.FACET_TYPE_ID)) return false;
+
         PsiClass psiClass = PsiTreeUtil.getTopmostParentOfType(element, PsiClass.class);
-        if (psiClass == null) return  false;
+        if (psiClass == null) return false;
 
         PsiFile file = psiClass.getContainingFile();
         if (file.getContainingDirectory() == null || JavaProjectRootsUtil.isOutsideJavaSourceRoot(file)) return false;
@@ -45,7 +48,7 @@ public class CreateATGComponentIntentionAction extends PsiElementBaseIntentionAc
     }
 
     @Override
-    public void invoke(final @NotNull Project project, Editor editor, @NotNull PsiElement element) throws IncorrectOperationException {
+    public void invoke(final @NotNull Project project, Editor editor, @NotNull PsiElement element) {
         final Module srcModule = ModuleUtilCore.findModuleForPsiElement(element);
         if (srcModule == null) return;
 
